a70a82f993dc969d080713050a87876e36c6f691
dokimiki <57443812+dokimiki@users.noreply.github.com>
2025-09-07 15:14:01 +0900

    wip

--- nextjs/src/app/map/page.tsx
+++ nextjs/src/app/map/page.tsx
@@ -106,7 +106,12 @@ function MapPin({ addCommentRef }: { addCommentRef: RefObject<((comment: string)
                 addCommentRef.current(`中央の緯度経度: ${center.lat.toFixed(4)}, ${center.lng.toFixed(4)}`);
                 (async () => {
                     const res = await fetch(
-                        `http://localhost:3030/pins?latMin=${bottomLeft.lat}&latMax=${topRight.lat}&lngMin=${bottomLeft.lng}&lngMax=${topRight.lng}`
+                        `http://localhost:3030/pins?latMin=${bottomLeft.lat}&latMax=${topRight.lat}&lngMin=${bottomLeft.lng}&lngMax=${topRight.lng}`,
+                        {
+                            headers: {
+                                Authorization: `Bearer ${localStorage.getItem("loginToken") || ""}`,
+                            },
+                        }
                     );
                     const data = await res.json();
                     console.log("Fetched pins:", data);
